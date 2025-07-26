c193015c31f990dcd95478502069580c9224ad8d
markusweiss <git@code71.de>
2025-07-26 19:45:39 +0200

    do a lot of pixel stuff, add start page

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const scaleFactor = 3;
 
 export const dialogueData = {
   pc: `PC lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  exit: `Exit Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  exit: `Hey do you really whant to <a href="index.html">leave</a>? I hope you will visit me again! <a href="index.html">Bye bye</a> and see you later!</a>`,
   note: `Note Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   sp: `Poster 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   re: `Poster 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
