8c1fde743db72201a6033bf113443af67ad50619
markusweiss <git@code71.de>
2025-07-26 14:58:09 +0200

    add more hints and more furniture

--- src/constants.js
+++ src/constants.js
@@ -3,4 +3,7 @@ export const scaleFactor = 3;
 export const dialogueData = {
   pc: `PC lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
   exit: `Exit Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  note: `Note Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  sp: `Poster 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  re: `Poster 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
 };
