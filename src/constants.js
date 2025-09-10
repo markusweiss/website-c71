5a8738e08412d5f5c7b6f93dcf7deda9f537337b
markusweiss <git@code71.de>
2025-09-10 14:41:52 +0200

    prepare for beta test launch

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,13 @@
 export const scaleFactor = 3;
 
 export const dialogueData = {
-  pc: `PC lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  exit: `Hey do you really whant to <a href="index.html">leave</a>? I hope you will visit me again! <a href="index.html">Bye bye</a> and see you later!</a>`,
-  note: `Note Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  sp: `Poster 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  re: `Poster 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  radio: `Radio Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  coffee: `CoffeeLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  fridge: `FridgeLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  games: `games 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+  pc: `Whant to see some of my programming stuff? Look at <a href="https://github.com/markusweiss" target="_blank" rel="noopener"> github</a> page or my <a href="https://codepen.io/markusweiss"" target="_blank" rel="noopener">CodePen</a> profile!`,
+  exit: `Hey do you really want to <a href="index.html">leave</a>? I hope you will visit me again! <a href="index.html">Bye bye</a> 👋 and see you later!</a>`,
+  note: `Whant to see my other profiles? Check out my <a href="https://www.linkedin.com/in/weissmarkus/" target="_blank" rel="noopener">LinkedIn</a> or <a href="https://www.xing.com/profile/Markus_Weiss6/" target="_blank" rel="noopener">XING</a> page.`,
+  sp: `Live long and prosper! 🖖 Hope you enjoy this page.`,
+  re: `Space, the infinite expanse. The year is 2025. Here is the launch 🚀 of my new website..`,
+  radio: `Hey turn the radio on! 🎵 Enjoy some nice background music while exploring my little world!`,
+  coffee: `Wanna contact me? Hey drink a coffee ☕️ and write me an <a href="mailto:infopost@code71.de">email</a> 💌`,
+  fridge: `Mhhh what should I eat today? Maybe a 🍔 or some 🍕? I think I will go for a salad 🥗!`,
+  games: `Want to see some gameJam 👾 Stuff? Check out my <a href="https://itch.io/profile/markusweiss" target="_blank" rel="noopener">itch.io profile</a>!`,
 };
